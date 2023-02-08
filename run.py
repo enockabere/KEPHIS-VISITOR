32e1ceabcb1c7a4eaca63fa5fec36115bd53d35d
enockabere <maebaenock95@gmail.com>
2023-02-08 13:31:26 +0300

    change visitor header

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from cryptography.fernet import Fernet
 
 def Encrypt():
+    """_summary_ FnVisitorsCard and field where that service appears
+    
+    LPO/LSO  = 1
+    Cheque = 2
+    Mpesa = 3
+
+    """
     key = Fernet.generate_key()
     f = Fernet(key)
     token = f.encrypt(b"1234567")
